c470709fcbb05d61553f4dfd6eeaca1586da9968
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-09-29 20:23:36 +0900

    exercise

--- 3.redux/redux/reducers/post.js
+++ 3.redux/redux/reducers/post.js
@@ -1,12 +1,19 @@
+const { produce } = require('immer')
+
 const initialState = []
 
+const ADD_POST = 'ADD_POST'
+
 const postReducer = (prevState = initialState, action) => {
-  switch (action.type) {
-    case 'ADD_POST':
-      return [...prevState, action.data]
-    default:
-      return prevState
-  }
+  return produce(prevState, draft => {
+    switch (action.type) {
+      case ADD_POST:
+        draft.push(action.data)
+        break
+      default:
+        break
+    }
+  })
 }
 
 module.exports = postReducer
--- 3.redux/redux/reducers/user.js
+++ 3.redux/redux/reducers/user.js
@@ -1,3 +1,5 @@
+const { produce } = require('immer')
+
 const initialState = { isLoggingIn: false, data: null }
 
 const LOG_IN_REQUEST = 'LOG_IN_REQUEST'
@@ -6,32 +8,27 @@ const LOG_IN_FAILURE = 'LOG_IN_FAILURE'
 const LOG_OUT = 'LOG_OUT'
 
 const userReducer = (prevState = initialState, action) => {
-  switch (action.type) {
-    case LOG_IN_REQUEST:
-      return {
-        ...prevState,
-        isLoggingIn: true,
-      }
-    case LOG_IN_SUCCESS:
-      return {
-        ...prevState,
-        isLoggingIn: false,
-        data: action.data,
-      }
-    case LOG_IN_FAILURE:
-      return {
-        ...prevState,
-        isLoggingIn: false,
-        data: null,
-      }
-    case LOG_OUT:
-      return {
-        ...prevState,
-        data: null,
-      }
-    default:
-      return prevState
-  }
+  return produce(prevState, draft => {
+    switch (action.type) {
+      case LOG_IN_REQUEST:
+        draft.data = null
+        draft.isLoggingIn = true
+        break
+      case LOG_IN_SUCCESS:
+        draft.isLoggingIn = false
+        draft.data = action.data
+        break
+      case LOG_IN_FAILURE:
+        draft.isLoggingIn = false
+        draft.data = null
+        break
+      case LOG_OUT:
+        draft.data = null
+        break
+      default:
+        break
+    }
+  })
 }
 
 module.exports = userReducer
